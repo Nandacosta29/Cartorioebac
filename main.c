8deb0dcf8997ea03504fea22ae2b96e450e905d7
Nandacosta29 <fernandagautney@gmail.com>
2025-05-05 17:44:01 -0300

    opção de saída
    incluso no sistema a opção de saída para o cliente.

--- main.c
+++ main.c
@@ -8,7 +8,80 @@
 #include <stdio.h> //biblioteca de comunicação com o usuário
 #include <stdlib.h> //biblioteca de alocação de espaço em memória
 #include <locale.h> // biblioteca de alocações de texto por região
+#include <string.h> //biblioteca responsável por cuidar das strings
 
+int registro(void) {
+    char arquivo[40], cpf[40], Nome[40], Sobrenome[40], Cargo[40];
+
+    printf("Digite o CPF a ser cadastrado: ");
+    scanf("%s", cpf);
+    strcpy(arquivo, cpf);
+
+    FILE *file = fopen(arquivo, "w");
+    fprintf(file, "%s,", cpf);
+    fclose(file);
+
+    printf("Digite o nome a ser cadastrado: ");
+    scanf("%s", Nome);
+    file = fopen(arquivo, "a");
+    fprintf(file, "%s,", Nome);
+    fclose(file);
+
+    printf("Digite o sobrenome a ser cadastrado: ");
+    scanf("%s", Sobrenome);
+    file = fopen(arquivo, "a");
+    fprintf(file, "%s,", Sobrenome);
+    fclose(file);
+
+    printf("Digite o cargo a ser cadastrado: ");
+    scanf("%s", Cargo);
+    file = fopen(arquivo, "a");
+    fprintf(file, "%s", Cargo);
+    fclose(file);
+
+    printf("Cadastro concluído!\nPressione Enter para continuar...");
+    getchar(); getchar();
+    return 0;
+}
+
+int consultar(void) {
+    setlocale(LC_ALL, "Portuguese");
+    char cpf[40], conteudo[200];
+
+    printf("Digite o CPF a ser consultado: ");
+    scanf("%s", cpf);
+
+    FILE *file = fopen(cpf, "r");
+    if (file == NULL) {
+        printf("Não foi possível abrir o arquivo. Usuário não encontrado.\n");
+    } else {
+        printf("\nEssas são as informações do usuário:\n");
+        while (fgets(conteudo, 200, file) != NULL) {
+            printf("%s\n", conteudo);
+        }
+        fclose(file);
+    }
+
+    printf("Pressione Enter para continuar...");
+    getchar(); getchar();
+    return 0;
+}
+
+int deletar(void) {
+    char cpf[40];
+    printf("Digite o CPF que deseja deletar: ");
+    scanf("%s", cpf);
+
+    if (remove(cpf) == 0) {
+        printf("Usuário removido com sucesso.\n");
+    } else {
+        printf("Usuário não encontrado ou erro ao remover.\n");
+    }
+
+    printf("Pressione Enter para continuar...");
+    getchar(); getchar();
+    return 0;
+}
 
 int main(void)
     {
